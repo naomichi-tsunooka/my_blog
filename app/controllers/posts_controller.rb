599487fdcfbc1773db094c12eacdc73d47a81770
naomichi-tsunooka <tsunookanaomichi@gmail.com>
2019-07-14 11:59:47 +0900

    リファクタリング終了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
-    @posts = Post.all
-    @new_posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
+    @new_posts = Post.all.order(created_at: :desc).limit(5)
   end
   def new
     @post = Post.new # フォーム用の空のインスタンスを生成する。
@@ -15,15 +16,15 @@ class PostsController < ApplicationController
       render :new
     end
   end
+
   def show
-  @post = Post.find(params[:id])
   end
+
   def edit
-  @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
+    
     if @post.update(post_params)
       redirect_to @post, notice: "ブログを更新しました。"
     else
@@ -31,7 +32,7 @@ class PostsController < ApplicationController
     end
   end
   def destroy
-    @post = Post.find(params[:id])
+    
     @post.destroy
     redirect_to posts_path, notice: "ブログを削除しちゃったよん。"
   end
@@ -40,6 +41,9 @@ class PostsController < ApplicationController
 
   def post_params # ストロングパラメータを定義する
     params.require(:post).permit(:title, :body)
+  end
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
 
